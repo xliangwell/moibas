30a0006ce244d1c539e6d0f1f500561e977379cf
xliangwell <42832606@qq.com>
2020-02-24 15:42:43 +0800

    xl

---
+++ src/main/java/com/moibas/moibas/MoibasApplication.java
@@ -0,0 +1,13 @@
+package com.moibas.moibas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoibasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoibasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moibas/moibas/MoibasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moibas.moibas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoibasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
